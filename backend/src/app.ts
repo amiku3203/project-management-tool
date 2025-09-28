5802ccd4db7ee1a9a4c853a5761c25df32a7d8ee
amiku3203 <kumaramit28538@gmail.com>
2025-09-28 23:27:43 +0530

    allow frontend url to backend cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://project-management-tool-dev.vercel.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded());
 
